8e89d4a2000df7c543a6e5f0c5c069509a21790c
George-F-Ramzi <109841377+George-F-Ramzi@users.noreply.github.com>
2023-07-28 15:47:04 +0300

    Fixed Import GetPayload Issue

--- package/index.ts
+++ package/index.ts
@@ -2,3 +2,4 @@ export * from "./client/authorizeClient";
 export * from "./client/authProvider";
 export * from "./client/getUser";
 export * from "./server/authenticate";
+export * from "./server/getPayload";
